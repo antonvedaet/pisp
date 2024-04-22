dcde9f556cc52e69ddccd4b69a64fee4bae77701
AntonSexov <antonvedernikov2003@gmail.com>
2024-04-22 14:01:04 +0300

    + implemented but only simple

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ with open("./pisp_code/helloWorld.pisp") as file:
 
 
 parsed_code = Parser.run(f)
-print(Translator.to_json("./json/jsonified.json", Translator.translate(parsed_code[0])))
+print(Translator.to_json("./json/jsonified.json", Translator.translate(parsed_code[1])))
--- translator.py
+++ translator.py
@@ -2,12 +2,17 @@
 import json
 
 class Translator:
+    
+    BYTE_MEMORY_START = 100
+    INT_MEMORY_START = 200
+    
     opcodes = {
                1:"LOAD",
                2: "STORE",
                3: "OUT",
                4: "JMP",
-               5: "LOADA"
+               5: "LOADA",
+               6: "ADD"
                #not finished
                }
     def string_to_bytes(s: str) -> list:
@@ -16,6 +21,7 @@ class Translator:
     def translate(a: list):
         res = []
         match a[0]:
+            
             case "print":
                 if a[1] == "\'":
                     #saving in memory
@@ -28,23 +34,42 @@ class Translator:
                         
                         res.append({"OPCODE" : 2,
                                     "INSTRUCTION": Translator.opcodes.get(2),
-                                    "OPERAND": hex(n+100)})
+                                    "OPERAND": hex(n + Translator.BYTE_MEMORY_START)})
                     #jump to start of string
                     res.append({"OPCODE" : 4,
                                 "INSTRUCTION": Translator.opcodes.get(4),
-                                "OPERAND": hex(100)})
+                                "OPERAND": hex(Translator.BYTE_MEMORY_START)})
                     #printing each byte
                     for n, _ in enumerate(" ".join(a[-1])):
                         
                         res.append({"OPCODE" : 5,
                                     "INSTRUCTION": Translator.opcodes.get(5),
-                                    "OPERAND": hex(n+100)})
+                                    "OPERAND": hex(n + Translator.BYTE_MEMORY_START)})
                         
                         res.append({"OPCODE" : 3,
                                     "INSTRUCTION": Translator.opcodes.get(3),
                                     "OPERAND": "NO_OPERAND"})
                         
-                        
+                if a[1] != "\'":
+                    #higher_order_function passed
+                    pass
+                
+                
+            case "+":
+                if all([isinstance(i, int) or len(i) == 1 for i in a]) and len(a) == 3:
+                    
+                    res.append({
+                        "OPCODE": 1,
+                        "INSTRUCTION": Translator.opcodes.get(1),
+                        "OPERAND": a[1]
+                    })
+                    
+                    res.append({
+                        "OPCODE": 6,
+                        "INSTRUCTION": Translator.opcodes.get(6),
+                        "OPERAND": a[2]
+                    })
+                    
             case _:
                 raise ValueError("Invalid pisp syntax")
         
