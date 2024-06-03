47b37fb3a4c30f1ea3fda84a804e264400d6579d
AntonSexov <antonvedernikov2003@gmail.com>
2024-06-03 20:58:34 +0300

    ALU

--- cpu/ALU.py
+++ cpu/ALU.py
@@ -1,15 +1,48 @@
+MIN_VALUE = -(2**32)
+MAX_VALUE = (2**32) - 1
+
 class ALU:
     
 
     def __init__(self):
-        self.result = 0
-        #flags
+        self.value = 0
+        #refresh_flags
         self.N = 0
         self.Z = 0
         self.C = 0 #если при сдвиге бита направо становиться единичкой то число нечетное
     
+    def refresh_flags(self):
+        if self.value == 0:
+            self.N = True
+        else:
+            self.N = False
+        
+        if self.value > MAX_VALUE or self.value < MIN_VALUE:
+            self.C = True
+        else:
+            self.C = False
+
+        if self.value < 0:
+            self.Z = True
+        else:
+            self.Z = False
+
     def add(self, a, b):
-        return a + b
+        self.value = a + b
+        self.refresh_flags()
     
     def sub(self, a, b):
-        return a - b
+        self.value = a - b
+        self.refresh_flags()
+
+    def div(self, a, b):
+        self.value = a / b
+        self.refresh_flags()
+
+    def mul(self, a, b):
+        self.value = a * b
+        self.refresh_flags()
+
+    def mod(self, a, b):
+        self.value = a % b
+        self.refresh_flags()
