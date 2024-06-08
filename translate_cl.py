e65a198b0ad8d86fca9c66c4390adcdb796212b6
AntonSexov <antonvedernikov2003@gmail.com>
2024-06-08 19:02:25 +0300

    tests fix

--- translate_cl.py
+++ translate_cl.py
@@ -2,5 +2,8 @@ import sys
 
 from translate import translate
 
-translate(sys.argv[1], sys.argv[2])
+if sys.argv[1] and sys.argv[2]:
+    translate(sys.argv[1], sys.argv[2])
+else:
+    print("Usage: translate.py <source_file> <target_file>")
 
